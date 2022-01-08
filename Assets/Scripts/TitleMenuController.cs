4452b72299657d72dc6cf8801c770b36653fb0c7
Tu Kien Vinh <vinh.tukien@gmail.com>
2022-01-09 01:29:32 +0700

    Update Title Scene: add handle for background music, resolution, full screen

--- Assets/Scripts/TitleMenuController.cs
+++ Assets/Scripts/TitleMenuController.cs
@@ -3,15 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class TitleMenuController : MonoBehaviour
 {
     public GameObject continueButton;
+
+    public AudioMixer audioMixer;
     
     // Start is called before the first frame update
     void Start()
     {
+        if (!PlayerPrefs.HasKey("resolution width"))
+        {
+            PlayerPrefs.SetInt("resolution width", Screen.currentResolution.width);
+            PlayerPrefs.SetInt("resolution height", Screen.currentResolution.height);
+        }
 
+        if (PlayerPrefs.HasKey("fullscreen"))
+        {
+            if (PlayerPrefs.GetInt("fullscreen") == 0)
+            {
+                Screen.SetResolution(PlayerPrefs.GetInt("resolution width"), PlayerPrefs.GetInt("resolution height"), false);
+            }
+            else
+            {
+                Screen.SetResolution(PlayerPrefs.GetInt("resolution width"), PlayerPrefs.GetInt("resolution height"), true);
+            }
+        }
+
+        if (PlayerPrefs.HasKey("bgVolume"))
+        {
+            audioMixer.SetFloat("volume", PlayerPrefs.GetFloat("bgVolume"));
+        }
+        else
+        {
+            audioMixer.SetFloat("volume", 0);
+        }
     }
 
     // Update is called once per frame
@@ -33,7 +62,18 @@ public class TitleMenuController : MonoBehaviour
         if (name.Equals("New Game"))
         {
             MyGameManager.Instance.isNewGame = true;
+
+            int width = PlayerPrefs.GetInt("resolution width");
+            int height = PlayerPrefs.GetInt("resolution height");
+            float volume = PlayerPrefs.GetFloat("bgVolume");
+            int fullscreen = PlayerPrefs.GetInt("fullscreen");
             PlayerPrefs.DeleteAll();
+            PlayerPrefs.SetInt("resolution width", width);
+            PlayerPrefs.SetInt("resolution height", height);
+            PlayerPrefs.SetFloat("bgVolume", volume);
+            PlayerPrefs.SetInt("fullscreen", fullscreen);
+            audioMixer.SetFloat("volume", volume);
+
             SceneManager.LoadScene("VillageScreen");
         }
         if (name.Equals("Continue"))
